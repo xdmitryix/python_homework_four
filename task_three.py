c625acda46f34419c55b15fc75c2ed0e89ff717c
dmitry <rbdeman@yandex.ru>
2023-02-08 23:49:14 +0300

    сделал доп задачу

--- task_three.py
+++ task_three.py
@@ -1,3 +1,25 @@
 # задача RLE необязательная. Реализуйте RLE алгоритм: реализуйте модуль сжатия и восстановления данных (где только буквы присутствуют для простоты).
 # например декодирование
 # https://stepik.org/lesson/21300/step/2
+
+
+# Sample Input:
+# 3ab4c2CaB
+
+# Sample Output:
+# aaabccccCCaB
+
+data_in = input('введи строку: ')
+text = list(data_in)
+text_len = len(text)
+resultlist = []
+for i in range(text_len):
+    count = 0
+    if text[i].isdigit():
+        count = int(text[i])
+        for k in range(count-1):  
+            resultlist.append(text[i+1])
+    else:
+        resultlist.append(text[i]) 
+res_string = ''.join(resultlist)
+print(res_string)
